bba263914b8a8abcb75208d966548110d6ae5f24
vcup <miryo@vcup.moe>
2022-04-21 14:29:40 +0800

    NotifyFilter_WatchingEventSetter_MustSameOfWatchingEvent

--- tests/MonitorFileSystem.Tests/WatcherTests.cs
+++ tests/MonitorFileSystem.Tests/WatcherTests.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using MonitorFileSystem.Monitor;
 using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
+using System.IO;
 
 namespace MonitorFileSystem.Test;
 
@@ -24,6 +25,25 @@ public class WatcherTests
         Assert.AreEqual(watcher.Monitoring, mustBe);
     }
 
+    [TestCase(WatchingEvent.FileName     , NotifyFilters.FileName)]
+    [TestCase(WatchingEvent.DirectoryName, NotifyFilters.DirectoryName)]
+    [TestCase(WatchingEvent.Attributes   , NotifyFilters.Attributes)]
+    [TestCase(WatchingEvent.Size         , NotifyFilters.Size)]
+    [TestCase(WatchingEvent.LastWrite    , NotifyFilters.LastWrite)]
+    [TestCase(WatchingEvent.LastAccess   , NotifyFilters.LastAccess)]
+    [TestCase(WatchingEvent.CreationTime , NotifyFilters.CreationTime)]
+    [TestCase(WatchingEvent.Security     , NotifyFilters.Security)]
+    public void NotifyFilter_WatchingEventSetter_MustSameOfWatchingEvent(WatchingEvent @event, NotifyFilters filters)
+    {
+        var factory = new TestFileSystemWatcher();
+        var fileSystem = new TestFileSystem(factory);
+
+        var wathcer = new Watcher("Tester", "./", "*", @event, fileSystem);
+
+        Assert.IsNotNull(factory.LastCreatedFileSystemWatcher);
+        Assert.AreEqual(factory.LastCreatedFileSystemWatcher!.NotifyFilter, filters);
+    }
+
     private class TestFileSystem : MockFileSystem
     {
         private readonly IFileSystemWatcherFactory fileSystemWatcherFactory;
@@ -34,5 +54,31 @@ public class WatcherTests
         }
 
         public override IFileSystemWatcherFactory FileSystemWatcher => fileSystemWatcherFactory;
+    }
+    
+    private class TestFileSystemWatcher : IFileSystemWatcherFactory
+    {
+        private IFileSystemWatcher? lastCreatedFileSystemWatcher;
+
+        public TestFileSystemWatcher()
+        {
+        }
+
+        public IFileSystemWatcher? LastCreatedFileSystemWatcher => lastCreatedFileSystemWatcher;
+
+        public IFileSystemWatcher CreateNew()
+        {
+            return lastCreatedFileSystemWatcher = lastCreatedFileSystemWatcher ?? new FileSystemWatcherWrapper();
+        }
+
+        public IFileSystemWatcher CreateNew(string path)
+        {
+            return lastCreatedFileSystemWatcher = lastCreatedFileSystemWatcher ?? new FileSystemWatcherWrapper(path);
+        }
+
+        public IFileSystemWatcher CreateNew(string path, string filter)
+        {
+            return lastCreatedFileSystemWatcher = lastCreatedFileSystemWatcher ?? new FileSystemWatcherWrapper(path, filter);
+        }
     }
 }
