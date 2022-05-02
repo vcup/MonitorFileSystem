d4c485a3d998a733875572bf657579ba4bb1fbbe
vcup <miryo@vcup.moe>
2022-05-02 18:14:57 +0800

    make Grpc Service Framework

---
+++ src/MonitorFileSystem/Grpc/GrpcStartup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MonitorFileSystem.Grpc;
+
+public class GrpcStartup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddGrpc();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+
+        });
+    }
+}
--- src/MonitorFileSystem/Program.cs
+++ src/MonitorFileSystem/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Hosting;
 using MonitorFileSystem;
-using MonitorFileSystem.Action;
 using MonitorFileSystem.Extensions;
+using MonitorFileSystem.Grpc;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddActions()
             .AddHostedService<Worker>();
+    })
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseStartup<GrpcStartup>();
     })
     .Build();
 
