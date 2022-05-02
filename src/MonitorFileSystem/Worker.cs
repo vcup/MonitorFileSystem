6ee228c9b6812d761a639162d8c0f308e041fa00
vcup <miryo@vcup.moe>
2022-05-03 00:23:25 +0800

    short .csproj

--- src/MonitorFileSystem/Worker.cs
+++ src/MonitorFileSystem/Worker.cs
@@ -13,20 +13,10 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var w = new MonitorManager
-        {
-            new Watcher("w1", "./", string.Empty),
-            new Watcher("w2", "./", string.Empty),
-            new Group("g1", string.Empty)
-        };
-
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            _logger.LogInformation("Watcher Count {0}", (w as ICollection<IWatcher>).Count);
-            _logger.LogInformation("Group Count {0}", (w as ICollection<IGroup>).Count);
-            _logger.LogInformation("Watcher Count {0}", w.Count);
-            await Task.Delay(1000, stoppingToken);
-        }
+       while (!stoppingToken.IsCancellationRequested)
+       {
+           _logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
+           await Task.Delay(1000, stoppingToken);
+       }
     }
 }
