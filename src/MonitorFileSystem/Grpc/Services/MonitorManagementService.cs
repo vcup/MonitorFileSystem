7b7a3c4aa70d958f5cb8f87dcc2a78d30164d7e3
vcup <miryo@vcup.moe>
2022-05-03 11:46:25 +0800

    Add More method for MonitorManagement

--- src/MonitorFileSystem/Grpc/Services/MonitorManagementService.cs
+++ src/MonitorFileSystem/Grpc/Services/MonitorManagementService.cs
@@ -36,4 +36,109 @@ public class MonitorManagementService : MonitorManagement.MonitorManagementBase
 
         return Task.FromResult(new Empty());
     }
+
+    public override Task<Empty> RemoveWatcher(WatcherRequest request, ServerCallContext context)
+    {
+        _manager.Remove(request.ToWatcher());
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> DisableWatcher(WatcherRequest request, ServerCallContext context)
+    {
+        request.ToWatcher(_manager).Monitoring = false;
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> EnableWatcher(WatcherRequest request, ServerCallContext context)
+    {
+        request.ToWatcher(_manager).Monitoring = true;
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> ToggleWatcher(WatcherRequest request, ServerCallContext context)
+    {
+        var watcher = request.ToWatcher(_manager);
+        watcher.Monitoring = !watcher.Monitoring;
+        return Task.FromResult(new Empty());
+    }
+
+    
+    public override Task<Empty> CreateGroup(GroupRequest request, ServerCallContext context)
+    {
+        _manager.Add(request.ToGroup());
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> RemoveGroup(GroupRequest request, ServerCallContext context)
+    {
+        _manager.Remove(request.ToGroup());
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> AddWatcherTo(WatcherAndGroupRequest request, ServerCallContext context)
+    {
+        request.Group.ToGroup(_manager).Add(request.Watcher.ToWatcher(_manager));
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> AddManyWatcherTo(ManyWatcherAndGroupRequest request, ServerCallContext context)
+    {
+        foreach (var watcher in request.Watchers)
+        {
+            request.Group.ToGroup(_manager).Add(watcher.ToWatcher(_manager));
+        }
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> AddWatcherToMany(WatcherAndManyGroupRequest request, ServerCallContext context)
+    {
+        foreach (var group in request.Groups)
+        {
+            group.ToGroup(_manager).Add(request.Watcher.ToWatcher(_manager));
+        }
+        return Task.FromResult(new Empty());
+    }
+    
+    public override Task<Empty> RemoveWatcherFor(WatcherAndGroupRequest request, ServerCallContext context)
+    {
+        request.Group.ToGroup(_manager).Remove(request.Watcher.ToWatcher(_manager));
+        return Task.FromResult(new Empty());
+    }
+    
+    public override Task<Empty> RemoveManyWatcherFor(ManyWatcherAndGroupRequest request, ServerCallContext context)
+    {
+        foreach (var watcher in request.Watchers)
+        {
+            request.Group.ToGroup(_manager).Remove(watcher.ToWatcher(_manager));
+        }
+        return Task.FromResult(new Empty());
+    }
+    
+    public override Task<Empty> RemoveWatcherForMany(WatcherAndManyGroupRequest request, ServerCallContext context)
+    {
+        foreach (var group in request.Groups)
+        {
+            group.ToGroup(_manager).Remove(request.Watcher.ToWatcher(_manager));
+        }
+        return Task.FromResult(new Empty());
+    }
+
+
+    public override Task<Empty> ClearUpAll(Empty request, ServerCallContext context)
+    {
+        _manager.ClearUp();
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> ClearWatcher(Empty request, ServerCallContext context)
+    {
+        (_manager as ICollection<IWatcher>).Clear();
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Empty> ClearGroup(Empty request, ServerCallContext context)
+    {
+        (_manager as ICollection<IGroup>).Clear();
+        return Task.FromResult(new Empty());
+    }
 }
