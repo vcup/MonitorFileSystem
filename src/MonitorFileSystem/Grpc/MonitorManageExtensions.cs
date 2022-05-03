390dbd889ae189be030825a64cace355f54cf815
vcup <miryo@vcup.moe>
2022-05-03 11:28:07 +0800

    Extension method for protocol buffers messages

---
+++ src/MonitorFileSystem/Grpc/MonitorManageExtensions.cs
@@ -0,0 +1,48 @@
+﻿using MonitorFileSystem.Monitor;
+
+namespace MonitorFileSystem.Grpc;
+
+public static class MonitorManageExtensions
+{
+    public static IWatcher ToWatcher(this WatcherRequest req)
+    {
+        switch (req.EventCase)
+        {
+            case WatcherRequest.EventOneofCase.None:
+                return new Watcher(req.Name, req.Path, req.Filter);
+            case WatcherRequest.EventOneofCase.EventValue:
+                return new Watcher(req.Name, req.Path, req.Filter, (WatchingEvent)req.EventValue);
+            default:
+                throw new ArgumentException("{Name} has unexpected .EventCase", nameof(req));
+        }
+    }
+
+    public static IWatcher ToWatcher(this WatcherRequest req, IMonitorManager manager)
+    {
+        var watcher = manager.FindWatcher(req.Name);
+
+        if (watcher is null)
+        {
+            manager.Add(watcher = req.ToWatcher());
+        }
+
+        return watcher;
+    }
+
+    public static IGroup ToGroup(this GroupRequest req)
+    {
+        return new Group(req.Name, req.Description);
+    }
+
+    public static IGroup ToGroup(this GroupRequest req, IMonitorManager manager)
+    {
+        var group = manager.FindGroup(req.Name);
+        
+        if (group is null)
+        {
+            manager.Add(group = req.ToGroup());
+        }
+
+        return group;
+    }
+}
