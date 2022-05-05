77820b700d807e476db7ccf1c1123315a75bed3d
vcup <miryo@vcup.moe>
2022-05-05 18:46:09 +0800

    fix some predictable issues

--- src/MonitorFileSystem/Action/Chain.cs
+++ src/MonitorFileSystem/Action/Chain.cs
@@ -8,6 +8,7 @@ internal class Chain : IChain
 {
     private readonly List<IOperate> _operates;
     private readonly List<IObserver<WatchingEventInfo>> _observers;
+    private readonly Dictionary<IOperate, IDisposable> _unsubscribes;
 
     public Chain(string name, string description = "", bool isReadOnly = false)
     {
@@ -17,6 +18,7 @@ internal class Chain : IChain
 
         _operates = new();
         _observers = new();
+        _unsubscribes = new();
     }
 
     public string Name { get; }
@@ -72,9 +74,19 @@ internal class Chain : IChain
 
     public IDisposable Subscribe(IObserver<WatchingEventInfo> observer)
     {
-        if (_observers.Contains(observer))
+        if (!_observers.Contains(observer))
         {
             _observers.Add(observer);
+        }
+
+        return new UnSubscribe<WatchingEventInfo>(_observers, observer);
+    }
+
+    private IDisposable SubscribeAt(int index, IObserver<WatchingEventInfo> observer)
+    {
+        if (!_observers.Contains(observer))
+        {
+            _observers.Insert(index, observer);
         }
 
         return new UnSubscribe<WatchingEventInfo>(_observers, observer);
@@ -97,10 +109,17 @@ internal class Chain : IChain
             return;
         }
         _operates.Add(item);
+        _unsubscribes.Add(item, Subscribe(item));
     }
 
     public void Clear()
     {
+        // this method can be make async
+        foreach (var operate in _operates)
+        {
+            _unsubscribes[operate].Dispose();
+        }
+        
         _operates.Clear();
     }
 
@@ -116,10 +135,11 @@ internal class Chain : IChain
 
     public bool Remove(IOperate item)
     {
-        if (IsReadOnly)
+        if (!Contains(item) || IsReadOnly)
         {
             return false;
         }
+        _unsubscribes[item].Dispose();
         return _operates.Remove(item);
     }
 
@@ -130,27 +150,67 @@ internal class Chain : IChain
         return _operates.IndexOf(item);
     }
 
+    /// <summary>
+    /// Insert a operate to index, if has not called <see cref="Chain.Subscribe"/>,
+    /// the operate will be called index-th when observed event
+    /// see also <see cref="IChain.Insert"/>
+    /// </summary>
+    /// <param name="index">index of item</param>
+    /// <param name="item">the operate</param>
+    /// <exception cref="ArgumentOutOfRangeException">index is out of range</exception>
     public void Insert(int index, IOperate item)
     {
+        if ((uint) index >= (uint) Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        
         if (IsReadOnly)
         {
             return;
         }
+
+        _unsubscribes.Add(item, _observers.Count == _operates.Count
+            ? SubscribeAt(index, item)
+            : Subscribe(item)
+        );
         _operates.Insert(index, item);
     }
 
     public void RemoveAt(int index)
     {
+        if ((uint)index >= (uint) Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        
         if (IsReadOnly)
         {
             return;
         }
+        _unsubscribes[this[index]].Dispose();
         _operates.RemoveAt(index);
     }
 
+    /// <summary>
+    /// same of <see cref="Chain.Insert"/>
+    /// see also <see cref="IChain.this"/>
+    /// </summary>
+    /// <param name="index">index of Operate</param>
+    /// <exception cref="InvalidOperationException">this Collection is ReadOnly</exception>
     public IOperate this[int index]
     {
         get => _operates[index];
-        set => _operates[index] = value;
+        set
+        {
+            if (IsReadOnly)
+            {
+                throw new InvalidOperationException("this collection is readonly");
+            }
+            
+            _unsubscribes[_operates[index]].Dispose();
+            _operates[index] = value;
+            Subscribe(value);
+        }
     }
 }
