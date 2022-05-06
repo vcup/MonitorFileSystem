074da2944a3fe828a26ce12e8596336f05bfcd2a
vcup <miryo@vcup.moe>
2022-05-06 14:40:21 +0800

    make Destination property for IMoveOperate

--- src/MonitorFileSystem/Action/IMoveOperate.cs
+++ src/MonitorFileSystem/Action/IMoveOperate.cs
@@ -4,6 +4,8 @@ namespace MonitorFileSystem.Action;
 
 public interface IMoveOperate : IOperate
 {
+    string Destination { get; set; }
+    
     void Initialization(string destination);
     
     void Initialization(Guid guid, string destination);
--- src/MonitorFileSystem/Action/MoveOperate.cs
+++ src/MonitorFileSystem/Action/MoveOperate.cs
@@ -5,9 +5,6 @@ namespace MonitorFileSystem.Action;
 
 public class MoveOperate : OperateBase, IMoveOperate
 {
-    // see Initialization()
-    private string _destination = null!;
-
     public MoveOperate(IFileSystem fileSystem, ILogger<MoveOperate> logger)
         : base(fileSystem, logger)
     {
@@ -19,6 +16,9 @@ public class MoveOperate : OperateBase, IMoveOperate
         throw new NotImplementedException("this Operate have not parameterless Initialization");
     }
 
+    // see Initialization()
+    public string Destination { get; set; } = null!;
+
     public void Initialization(string destination)
     {
         Initialization(Guid.NewGuid(), destination);
@@ -28,21 +28,21 @@ public class MoveOperate : OperateBase, IMoveOperate
     {
         CheckIsNotInitialized();
         Guid = guid;
-        _destination = destination;
+        Destination = destination;
         IsInitialized = true;
     }
     
     public override void Process(WatchingEventInfo info)
     {
         base.Process(info);
-        var dest = FileSystem.Directory.Exists(_destination)
-            ? FileSystem.Path.Join(_destination, info.Path)
-            : _destination;
+        var dest = FileSystem.Directory.Exists(Destination)
+            ? FileSystem.Path.Join(Destination, info.Path)
+            : Destination;
              
         if (FileSystem.File.Exists(info.Path))
         {
            
-            FileSystem.File.Move(info.Path, _destination, true);
+            FileSystem.File.Move(info.Path, Destination, true);
             // if destIsFile is null, dest path also is a File. because a file is Moved to dest path
             
             Logger.LogTrace("MoveOperate Process on File branch, {path} -> {dest}", info.Path, dest);
