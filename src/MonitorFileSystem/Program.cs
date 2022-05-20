2c15e84d16b0e54181a37cebef662c0a9ce8a5b6
vcup <miryo@vcup.moe>
2022-05-21 00:07:08 +0800

    let command can temp use specify configuration file

--- src/MonitorFileSystem/Program.cs
+++ src/MonitorFileSystem/Program.cs
@@ -1,3 +1,6 @@
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
 using Microsoft.AspNetCore.Hosting;
 using MonitorFileSystem.Grpc;
 
@@ -6,24 +9,6 @@ var builder = new HostBuilder()
     .ConfigureHostConfiguration(config =>
     {
         config.AddEnvironmentVariables("MFS_"); // MFS -> MonitorFileSystem
-    })
-    .ConfigureAppConfiguration((context, config) =>
-    {
-        const string configName = "settings.json";
-        var configNameWithEnv = "settings." + context.HostingEnvironment.EnvironmentName + ".json";
-        var userConfig = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-            ".config", "monitorfs");
-        config
-#if Windows
-            .AddJsonFile(configName, true, true)
-            .AddJsonFile(configNameWithEnv, true, true)
-#elif Linux
-            .AddJsonFile(configName, true, true)
-            .AddJsonFile(configNameWithEnv, true, true)
-#endif
-            .AddJsonFile(userConfig + configName, true, true)
-            .AddJsonFile(userConfig + configNameWithEnv, true, true)
-            ;
     })
     .ConfigureLogging((context, logging) =>
     {
@@ -35,6 +20,55 @@ var builder = new HostBuilder()
         webBuilder.UseStartup<GrpcStartup>();
     });
 
-using var host = builder.Build();
+var rootCommand = new RootCommand();
 
-await host.RunAsync();
+var configPath = new Option<string>("--conf")
+{
+    Arity = ArgumentArity.ZeroOrOne
+};
+
+rootCommand.AddOption(configPath);
+rootCommand.SetHandler(async () =>
+{
+    using var host = builder.Build();
+    await host.RunAsync();
+});
+
+
+var commandBuilder = new CommandLineBuilder(rootCommand);
+commandBuilder.AddMiddleware(content  =>
+{
+    if (content.ParseResult.HasOption(configPath))
+    {
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddJsonFile(content.ParseResult.GetValueForOption(configPath));
+        });
+    }
+    else
+    {
+        builder.ConfigureAppConfiguration(UseDefaultConfigures);
+    }
+});
+
+var parser = commandBuilder.Build();
+await parser.InvokeAsync(args);
+
+void UseDefaultConfigures(HostBuilderContext context,IConfigurationBuilder config)
+{
+    const string configName = "settings.json";
+    var configNameWithEnv = "settings." + context.HostingEnvironment.EnvironmentName + ".json";
+    var userConfig = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+        ".config", "monitorfs");
+    config
+#if Windows
+        .AddJsonFile(configName, true, true)
+        .AddJsonFile(configNameWithEnv, true, true)
+#elif Linux
+        .AddJsonFile(configName, true, true)
+        .AddJsonFile(configNameWithEnv, true, true)
+#endif
+        .AddJsonFile(userConfig + configName, true, true)
+        .AddJsonFile(userConfig + configNameWithEnv, true, true)
+        ;
+}
